43e727729d3c223068547add0d6450de841151c4
Kosorukov Anatolii <java1cprog@gmail.com>
2026-06-14 19:58:43 +0700

    Add pagination and user-specific post display: update home route to paginate posts and create user_posts template for displaying posts by a specific user.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,13 +13,12 @@ from .forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 bp = Blueprint("blog", __name__)
 
 
-
-
 @bp.route("/")
 @bp.route("/home")
 def home():
     """Return a simple greeting message."""
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("home.html", posts=posts)
 
 
@@ -164,3 +163,15 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your post has been deleted!", "success")
     return redirect(url_for("blog.home"))
+
+
+@bp.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = (
+        Post.query.filter_by(author=user)
+        .order_by(Post.date_posted.desc())
+        .paginate(page=page, per_page=5)
+    )
+    return render_template("user_posts.html", posts=posts, user=user)
