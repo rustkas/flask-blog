0b02af79c724da49943b99e61bf7029021a215ec
Kosorukov Anatolii <java1cprog@gmail.com>
2026-06-14 21:28:49 +0700

    Refactor Flask Blog Application Structure
    - Moved configuration settings to a new config.py file for better organization.
    - Updated __init__.py to utilize the new Config class for app configuration.
    - Removed the old routes.py file and split routes into separate blueprints for users, posts, and main functionalities.
    - Created new forms.py files for user and post forms, encapsulating form logic.
    - Added utility functions for saving pictures and sending reset emails in a new utils.py file.
    - Updated templates to reflect new blueprint routes and improved structure.
    - Removed unused forms and consolidated form logic into respective modules.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,35 +1,33 @@
-"""A simple Flask application that returns "Hello World!" when accessed at the root URL."""
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
+
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+login_manager.login_view = 'users.login' # type: ignore
+login_manager.login_message_category = 'info'
 mail = Mail()
 
-def create_app():
-    """Create app"""
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "ddb6bc214b788a8a7711ad104220d603"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-    db.init_app(app)
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'blog.login' # type: ignore
-    login_manager.login_message_category = 'info'
-    
-    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-    app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
     mail.init_app(app)
 
-    from . import routes
-    app.register_blueprint(routes.bp)
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
     return app
